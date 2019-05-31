c2430650cc4f8499e46c3b136cd3880966178366
Antoine Lafarge <ant.lafarge@gmail.com>
2019-05-31 02:05:31 +0200

    fix macro, add vs project files

--- JsonSerializer.h
+++ JsonSerializer.h
@@ -64,24 +64,10 @@
 #define FOR_EACH_48(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_47(what,  __VA_ARGS__))
 #define FOR_EACH_49(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_48(what,  __VA_ARGS__))
 #define FOR_EACH_50(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_49(what,  __VA_ARGS__))
-#define FOR_EACH_51(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_50(what,  __VA_ARGS__))
-#define FOR_EACH_52(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_51(what,  __VA_ARGS__))
-#define FOR_EACH_53(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_52(what,  __VA_ARGS__))
-#define FOR_EACH_54(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_53(what,  __VA_ARGS__))
-#define FOR_EACH_55(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_54(what,  __VA_ARGS__))
-#define FOR_EACH_56(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_55(what,  __VA_ARGS__))
-#define FOR_EACH_57(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_56(what,  __VA_ARGS__))
-#define FOR_EACH_58(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_57(what,  __VA_ARGS__))
-#define FOR_EACH_59(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_58(what,  __VA_ARGS__))
-#define FOR_EACH_60(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_59(what,  __VA_ARGS__))
-#define FOR_EACH_61(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_60(what,  __VA_ARGS__))
-#define FOR_EACH_62(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_61(what,  __VA_ARGS__))
-#define FOR_EACH_63(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_62(what,  __VA_ARGS__))
-#define FOR_EACH_64(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_63(what,  __VA_ARGS__))
 #define FOR_EACH_NARG(...) FOR_EACH_NARG_(__VA_ARGS__, FOR_EACH_RSEQ_N())
 #define FOR_EACH_NARG_(...) EXPAND(FOR_EACH_ARG_N(__VA_ARGS__))
-#define FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, N, ...) N
-#define FOR_EACH_RSEQ_N() 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, N, ...) N
+#define FOR_EACH_RSEQ_N() 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 #define CONCATENATE(x,y) x##y
 #define FOR_EACH_(N, what, ...) EXPAND(CONCATENATE(FOR_EACH_, N)(what, __VA_ARGS__))
 #define FOR_EACH(what, ...) FOR_EACH_(FOR_EACH_NARG(__VA_ARGS__), what, __VA_ARGS__)
