401acacd8c1df6238701364ef9b7e61538341007
Antoine Lafarge <ant.lafarge@gmail.com>
2019-05-31 02:42:50 +0200

    Support 0 variadic arguments, Support non-microsoft compilers

--- JsonSerializer.h
+++ JsonSerializer.h
@@ -11,79 +11,85 @@
 #include <unordered_map>
 #include <cpprest/json.h>
 
-#define FOR_EACH_SEP ,
 #define EXPAND(x) x
 #define FOR_EACH_0(what, x, ...)
 #define FOR_EACH_1(what, x, ...) what(x)
-#define FOR_EACH_2(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_1(what,  __VA_ARGS__))
-#define FOR_EACH_3(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_2(what, __VA_ARGS__))
-#define FOR_EACH_4(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_3(what,  __VA_ARGS__))
-#define FOR_EACH_5(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_4(what,  __VA_ARGS__))
-#define FOR_EACH_6(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_5(what,  __VA_ARGS__))
-#define FOR_EACH_7(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_6(what,  __VA_ARGS__))
-#define FOR_EACH_8(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_7(what,  __VA_ARGS__))
-#define FOR_EACH_9(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_8(what,  __VA_ARGS__))
-#define FOR_EACH_10(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_9(what,  __VA_ARGS__))
-#define FOR_EACH_11(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_10(what,  __VA_ARGS__))
-#define FOR_EACH_12(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_11(what,  __VA_ARGS__))
-#define FOR_EACH_13(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_12(what,  __VA_ARGS__))
-#define FOR_EACH_14(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_13(what,  __VA_ARGS__))
-#define FOR_EACH_15(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_14(what,  __VA_ARGS__))
-#define FOR_EACH_16(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_15(what,  __VA_ARGS__))
-#define FOR_EACH_17(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_16(what,  __VA_ARGS__))
-#define FOR_EACH_18(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_17(what,  __VA_ARGS__))
-#define FOR_EACH_19(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_18(what,  __VA_ARGS__))
-#define FOR_EACH_20(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_19(what,  __VA_ARGS__))
-#define FOR_EACH_21(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_20(what,  __VA_ARGS__))
-#define FOR_EACH_22(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_21(what,  __VA_ARGS__))
-#define FOR_EACH_23(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_22(what,  __VA_ARGS__))
-#define FOR_EACH_24(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_23(what,  __VA_ARGS__))
-#define FOR_EACH_25(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_24(what,  __VA_ARGS__))
-#define FOR_EACH_26(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_25(what,  __VA_ARGS__))
-#define FOR_EACH_27(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_26(what,  __VA_ARGS__))
-#define FOR_EACH_28(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_27(what,  __VA_ARGS__))
-#define FOR_EACH_29(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_28(what,  __VA_ARGS__))
-#define FOR_EACH_30(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_29(what,  __VA_ARGS__))
-#define FOR_EACH_31(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_30(what,  __VA_ARGS__))
-#define FOR_EACH_32(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_31(what,  __VA_ARGS__))
-#define FOR_EACH_33(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_32(what,  __VA_ARGS__))
-#define FOR_EACH_34(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_33(what,  __VA_ARGS__))
-#define FOR_EACH_35(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_34(what,  __VA_ARGS__))
-#define FOR_EACH_36(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_35(what,  __VA_ARGS__))
-#define FOR_EACH_37(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_36(what,  __VA_ARGS__))
-#define FOR_EACH_38(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_37(what,  __VA_ARGS__))
-#define FOR_EACH_39(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_38(what,  __VA_ARGS__))
-#define FOR_EACH_40(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_39(what,  __VA_ARGS__))
-#define FOR_EACH_41(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_40(what,  __VA_ARGS__))
-#define FOR_EACH_42(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_41(what,  __VA_ARGS__))
-#define FOR_EACH_43(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_42(what,  __VA_ARGS__))
-#define FOR_EACH_44(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_43(what,  __VA_ARGS__))
-#define FOR_EACH_45(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_44(what,  __VA_ARGS__))
-#define FOR_EACH_46(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_45(what,  __VA_ARGS__))
-#define FOR_EACH_47(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_46(what,  __VA_ARGS__))
-#define FOR_EACH_48(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_47(what,  __VA_ARGS__))
-#define FOR_EACH_49(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_48(what,  __VA_ARGS__))
-#define FOR_EACH_50(what, x, ...) what(x)FOR_EACH_SEP EXPAND(FOR_EACH_49(what,  __VA_ARGS__))
-#define FOR_EACH_NARG(...) FOR_EACH_NARG_(__VA_ARGS__, FOR_EACH_RSEQ_N())
-#define FOR_EACH_NARG_(...) EXPAND(FOR_EACH_ARG_N(__VA_ARGS__))
-#define FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, N, ...) N
-#define FOR_EACH_RSEQ_N() 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define FOR_EACH_2(what, x, ...) what(x) EXPAND(FOR_EACH_1(what,  __VA_ARGS__))
+#define FOR_EACH_3(what, x, ...) what(x) EXPAND(FOR_EACH_2(what, __VA_ARGS__))
+#define FOR_EACH_4(what, x, ...) what(x) EXPAND(FOR_EACH_3(what,  __VA_ARGS__))
+#define FOR_EACH_5(what, x, ...) what(x) EXPAND(FOR_EACH_4(what,  __VA_ARGS__))
+#define FOR_EACH_6(what, x, ...) what(x) EXPAND(FOR_EACH_5(what,  __VA_ARGS__))
+#define FOR_EACH_7(what, x, ...) what(x) EXPAND(FOR_EACH_6(what,  __VA_ARGS__))
+#define FOR_EACH_8(what, x, ...) what(x) EXPAND(FOR_EACH_7(what,  __VA_ARGS__))
+#define FOR_EACH_9(what, x, ...) what(x) EXPAND(FOR_EACH_8(what,  __VA_ARGS__))
+#define FOR_EACH_10(what, x, ...) what(x) EXPAND(FOR_EACH_9(what,  __VA_ARGS__))
+#define FOR_EACH_11(what, x, ...) what(x) EXPAND(FOR_EACH_10(what,  __VA_ARGS__))
+#define FOR_EACH_12(what, x, ...) what(x) EXPAND(FOR_EACH_11(what,  __VA_ARGS__))
+#define FOR_EACH_13(what, x, ...) what(x) EXPAND(FOR_EACH_12(what,  __VA_ARGS__))
+#define FOR_EACH_14(what, x, ...) what(x) EXPAND(FOR_EACH_13(what,  __VA_ARGS__))
+#define FOR_EACH_15(what, x, ...) what(x) EXPAND(FOR_EACH_14(what,  __VA_ARGS__))
+#define FOR_EACH_16(what, x, ...) what(x) EXPAND(FOR_EACH_15(what,  __VA_ARGS__))
+#define FOR_EACH_17(what, x, ...) what(x) EXPAND(FOR_EACH_16(what,  __VA_ARGS__))
+#define FOR_EACH_18(what, x, ...) what(x) EXPAND(FOR_EACH_17(what,  __VA_ARGS__))
+#define FOR_EACH_19(what, x, ...) what(x) EXPAND(FOR_EACH_18(what,  __VA_ARGS__))
+#define FOR_EACH_20(what, x, ...) what(x) EXPAND(FOR_EACH_19(what,  __VA_ARGS__))
+#define FOR_EACH_21(what, x, ...) what(x) EXPAND(FOR_EACH_20(what,  __VA_ARGS__))
+#define FOR_EACH_22(what, x, ...) what(x) EXPAND(FOR_EACH_21(what,  __VA_ARGS__))
+#define FOR_EACH_23(what, x, ...) what(x) EXPAND(FOR_EACH_22(what,  __VA_ARGS__))
+#define FOR_EACH_24(what, x, ...) what(x) EXPAND(FOR_EACH_23(what,  __VA_ARGS__))
+#define FOR_EACH_25(what, x, ...) what(x) EXPAND(FOR_EACH_24(what,  __VA_ARGS__))
+#define FOR_EACH_26(what, x, ...) what(x) EXPAND(FOR_EACH_25(what,  __VA_ARGS__))
+#define FOR_EACH_27(what, x, ...) what(x) EXPAND(FOR_EACH_26(what,  __VA_ARGS__))
+#define FOR_EACH_28(what, x, ...) what(x) EXPAND(FOR_EACH_27(what,  __VA_ARGS__))
+#define FOR_EACH_29(what, x, ...) what(x) EXPAND(FOR_EACH_28(what,  __VA_ARGS__))
+#define FOR_EACH_30(what, x, ...) what(x) EXPAND(FOR_EACH_29(what,  __VA_ARGS__))
+#define FOR_EACH_31(what, x, ...) what(x) EXPAND(FOR_EACH_30(what,  __VA_ARGS__))
+#define FOR_EACH_32(what, x, ...) what(x) EXPAND(FOR_EACH_31(what,  __VA_ARGS__))
+#define FOR_EACH_33(what, x, ...) what(x) EXPAND(FOR_EACH_32(what,  __VA_ARGS__))
+#define FOR_EACH_34(what, x, ...) what(x) EXPAND(FOR_EACH_33(what,  __VA_ARGS__))
+#define FOR_EACH_35(what, x, ...) what(x) EXPAND(FOR_EACH_34(what,  __VA_ARGS__))
+#define FOR_EACH_36(what, x, ...) what(x) EXPAND(FOR_EACH_35(what,  __VA_ARGS__))
+#define FOR_EACH_37(what, x, ...) what(x) EXPAND(FOR_EACH_36(what,  __VA_ARGS__))
+#define FOR_EACH_38(what, x, ...) what(x) EXPAND(FOR_EACH_37(what,  __VA_ARGS__))
+#define FOR_EACH_39(what, x, ...) what(x) EXPAND(FOR_EACH_38(what,  __VA_ARGS__))
+#define FOR_EACH_40(what, x, ...) what(x) EXPAND(FOR_EACH_39(what,  __VA_ARGS__))
+#define FOR_EACH_41(what, x, ...) what(x) EXPAND(FOR_EACH_40(what,  __VA_ARGS__))
+#define FOR_EACH_42(what, x, ...) what(x) EXPAND(FOR_EACH_41(what,  __VA_ARGS__))
+#define FOR_EACH_43(what, x, ...) what(x) EXPAND(FOR_EACH_42(what,  __VA_ARGS__))
+#define FOR_EACH_44(what, x, ...) what(x) EXPAND(FOR_EACH_43(what,  __VA_ARGS__))
+#define FOR_EACH_45(what, x, ...) what(x) EXPAND(FOR_EACH_44(what,  __VA_ARGS__))
+#define FOR_EACH_46(what, x, ...) what(x) EXPAND(FOR_EACH_45(what,  __VA_ARGS__))
+#define FOR_EACH_47(what, x, ...) what(x) EXPAND(FOR_EACH_46(what,  __VA_ARGS__))
+#define FOR_EACH_48(what, x, ...) what(x) EXPAND(FOR_EACH_47(what,  __VA_ARGS__))
+#define FOR_EACH_49(what, x, ...) what(x) EXPAND(FOR_EACH_48(what,  __VA_ARGS__))
+#define FOR_EACH_50(what, x, ...) what(x) EXPAND(FOR_EACH_49(what,  __VA_ARGS__))
+
+#ifdef _MSC_VER // Microsoft compilers
+#   define ARGS_COUNT(...) EXPAND_ARGS_PRIVATE(ARGS_AUGMENTER(__VA_ARGS__))
+#   define ARGS_AUGMENTER(...) unused, __VA_ARGS__
+#   define EXPAND_ARGS_PRIVATE(...) EXPAND(ARGS_COUNT_PRIVATE(__VA_ARGS__, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
+#   define ARGS_COUNT_PRIVATE(_1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, _17_, _18_, _19_, _20_, _21_, _22_, _23_, _24_, _25_, _26_, _27_, _28_, _29_, _30_, _31_, _32_, _33_, _34_, _35_, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, count, ...) count
+#else // Non-Microsoft compilers
+#   define ARGS_COUNT(...) ARGS_COUNT_PRIVATE(0, ## __VA_ARGS__, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#   define ARGS_COUNT_PRIVATE(_0, _1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, _17_, _18_, _19_, _20_, _21_, _22_, _23_, _24_, _25_, _26_, _27_, _28_, _29_, _30_, _31_, _32_, _33_, _34_, _35_, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, count, ...) count
+#endif
+
 #define CONCATENATE(x,y) x##y
 #define FOR_EACH_(N, what, ...) EXPAND(CONCATENATE(FOR_EACH_, N)(what, __VA_ARGS__))
-#define FOR_EACH(what, ...) FOR_EACH_(FOR_EACH_NARG(__VA_ARGS__), what, __VA_ARGS__)
+#define FOR_EACH(what, ...) FOR_EACH_(ARGS_COUNT(__VA_ARGS__), what, __VA_ARGS__)
 
-#define JSON_ADD_FIELD_NAME(A) #A, A
+#define JSON_ADD_FIELD_NAME(A) , #A, A
 
 #define JSON_SERIALIZER(...) \
 operator web::json::value() const \
 { \
 	web::json::value jsonValue; \
-	JsonSerializer::JsonSerializerImpl::serialize(jsonValue, FOR_EACH(JSON_ADD_FIELD_NAME, __VA_ARGS__)); \
+	JsonSerializer::JsonSerializerImpl::serialize(jsonValue FOR_EACH(JSON_ADD_FIELD_NAME, __VA_ARGS__)); \
 	return jsonValue; \
 } \
 void deserializeFromJson(const web::json::value& jsonValue) \
 { \
-	JsonSerializer::JsonSerializerImpl::deserialize(jsonValue, FOR_EACH(JSON_ADD_FIELD_NAME, __VA_ARGS__)); \
+	JsonSerializer::JsonSerializerImpl::deserialize(jsonValue FOR_EACH(JSON_ADD_FIELD_NAME, __VA_ARGS__)); \
 }
 
 const std::string errorSerializationMsg = "Json serialization failed";
